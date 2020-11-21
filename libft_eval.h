b2fc5d2cbcac78e2497136f49459379c28cba1aa
root <root@PC-Vincent.localdomain>
2020-11-21 12:51:35 +0100

    typo

--- libft_eval.h
+++ libft_eval.h
@@ -6,7 +6,7 @@
 /*   By: vbotte <vbotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/19 14:02:38 by vbotte            #+#    #+#             */
-/*   Updated: 2020/11/21 12:45:56 by vbotte           ###   ########.fr       */
+/*   Updated: 2020/11/21 12:50:38 by vbotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,4 @@ char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_itoa(int n);
 char			*ft_strtrim(char const *s1, char const *set);
 char			**ft_split(char const *s, char c);
-char			*ft_strmapi(char const *s, char (*f)(unisgned int, char));
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
