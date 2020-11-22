0b646ac8939bc61f02b88be79c4532108f166803
Elxse <elise.chin@hotmail.fr>
2020-11-22 21:36:25 +0100

    Add tests for ft_strjoin

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,16 +6,24 @@
 /*   By: vbotte <vbotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 04:11:06 by vbotte            #+#    #+#             */
-/*   Updated: 2020/11/20 21:22:01 by vbotte           ###   ########.fr       */
+/*   Updated: 2020/11/22 21:32:30 by echin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft_eval.h"
 
+void	test_ft_strjoin_basic(void)
+{
+	char str1[] = "Hi!";
+	char str2[] = "How are you ";
+	char str3[] = "today?";
+	printf("User		: %s\n", ft_strjoin(ft_strjoin(str1, str2), str3));
+	printf("Expected	: Hi! How are you today?\n\n");
+}
+
 int main()
 {
-	char str1[] = "str1 ";
-	char str2[] = "str2";
-	printf("User		: %s\nExpected	: str1 str2\n", ft_strjoin(str1, str2));
-	return 0;
+	printf("===== 1. Basic input =====\n");
+	test_ft_strjoin_basic();
+	return (0);
 }
